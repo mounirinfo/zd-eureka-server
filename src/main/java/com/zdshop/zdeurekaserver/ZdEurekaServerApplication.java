faee105d709f164ff97b05a7a78c35a786246b61
Mounir ZAGHBA <zaghba.mounir@gmail.com>
2020-08-25 23:17:24 +0200

    first commit

---
+++ src/main/java/com/zdshop/zdeurekaserver/ZdEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zdshop.zdeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zdshop/zdeurekaserver/ZdEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdshop.zdeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
